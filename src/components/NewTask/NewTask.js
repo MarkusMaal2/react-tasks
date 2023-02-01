b4d09e1e3de90a50b67d09218a940acb8a6bdc6b
MarkusMaal2 <markus.maal@voco.ee>
2023-02-01 13:22:38 +0200

    Reworked NewExpense (from my_expenses) to NewTask component

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,10 +1,25 @@
-import React from "react";
-import TaskForm from './TaskForm'
+import React, {useState} from "react"
+import './NewTask.css'
+
+import TaskForm from "./TaskForm"
 
 const NewTask = (props) => {
+    const [addFormVisible, setAddForm] = useState(false)
+    const saveTaskDataHandler = (enteredTaskData) => {
+        const taskData = {
+            ...enteredTaskData,
+            id: Math.random().toString()
+        }
+        props.onAddTask(taskData)
+    }
+    const toggleFormHandler = () => {
+        setAddForm(!addFormVisible)
+    }
+
     return (
-        <div><p>NewTask</p>
-        <TaskForm></TaskForm></div>
+        <div className="new-expense">
+            <TaskForm onToggleForm={toggleFormHandler} onSaveTaskData={saveTaskDataHandler} formVisible={addFormVisible}></TaskForm>
+        </div>
     )
 }
 
